7c212112cfeb20ad948beb0d193e526fbe53ab07
Raditya Tresna Dwimurti <raditiya.tresna.dwimurti18@gmail.com>
2026-04-15 10:13:03 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:match_discovery/firebase_options.dart';
-import 'package:match_discovery/login/login.dart';
 import 'package:match_discovery/view/splash.dart';
 
 void main() async {
