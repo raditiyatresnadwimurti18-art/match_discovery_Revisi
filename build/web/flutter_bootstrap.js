b395edc5cab73cd84d2b26dfa3791798f47c59e8
Raditya Tresna Dwimurti <raditiya.tresna.dwimurti18@gmail.com>
2026-05-17 17:21:10 +0700

    Trigger update ke link utama Vercel

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "118105012" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "602122874" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
